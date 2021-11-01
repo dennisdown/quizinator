d9d6cbdb558459458b0610a59ef0f3acd6b30536
Dennis <dennisrdown@gmail.com>
2021-11-01 00:35:51 -0400

    intial quiz is complete. need to add timer and high score

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,6 +8,10 @@ let answerButtonsElement = document.getElementById('answer-buttons')
 let shuffledQuestions, currentQuestionIndex
 
 startButton.addEventListener('click', startGame)
+nextButton.addEventListener('click', () => {
+  currentQuestionIndex++
+  setNextQuestion()
+})
 
 
 function startGame() {
@@ -49,7 +53,29 @@ function selectAnswer(e) {
   let selectedButton = e.target 
   let correct = selectedButton.dataset.correct
   setStatusClass(document.body, correct)
-  Array.from(answerButtonsElement.children)
+  Array.from(answerButtonsElement.children).forEach(button => {
+    setStatusClass(button, button.dataset.correct)
+  })
+  if (shuffledQuestions.length > currentQuestionIndex + 1) {
+    nextButton.classList.remove('hide')
+  } else {
+    startButton.innerText = 'Restart'
+    startButton.classList.remove('hide')
+  }
+}
+
+function setStatusClass(element, correct) {
+  clearStatusClass(element)
+  if (correct) {
+    element.classList.add('correct')
+  } else {
+    element.classList.add('wrong')
+  }
+}
+
+function clearStatusClass(element) {
+  element.classList.remove('correct')
+  element.classList.remove('wrong')
 }
 
  let questions = [
@@ -61,5 +87,32 @@ function selectAnswer(e) {
       {text: '<js>', correct: false },
       {text: '<scripture>', correct: false }
      ]
-   }
- ]
+   },
+   {
+     question: 'How do you creat a function in JavaScript?',
+     answers: [
+      {text: 'function === myfunction', correct: false },
+      {text: 'function.myFunction', correct: false },
+      {text: 'function myFunction()', correct: true },
+      {text: 'function: myfunction()', correct: false }
+     ]
+   },
+   {
+     question: 'How can you add a comment in JavaScript',
+     answers: [
+      {text: '"This is a comment"', correct: false },
+      {text: '// This is a comment', correct: true },
+      {text: '<--This is a comment-->', correct: false },
+      {text: '<-*This is a comment', correct: false }
+     ]
+   },
+   {
+     question: 'What is the correct way to write an array?',
+     answers: [
+      {text: 'let colors = 1.)red 2.)blue 3.)green', correct: false },
+      {text: 'let colors = "red", "blue", "green"', correct: false },
+      {text: 'let colors = ["red", "blue", "green"]', correct: true },
+      {text: 'var colors = rgb(red,blue,green)', correct: false }
+     ]
+   },
+  ]
